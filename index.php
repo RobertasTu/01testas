45fcc99f6023fa091f508bcb0c8e29769f5aa45a
RobertasTu <tumelis.robertas@gmail.com>
2024-07-26 12:02:33 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01Testas</title>
+    <style>
+        body { background-color: beige;
+        }
+
+    </style>
+</head>
+<body>
+
+
+    <?php
+    //Testeris
+    $skA = 5;
+    $skB = 8;
+    $suma = $skA + $skB;
+
+    echo $skA.", ". $skB.", ".$suma;
+        echo "<br>";
+    echo "$skA Kintamasis A; ";
+    echo "$skB Kintamasis B; ";
+    
+    echo "Kintamuju suma: $suma ;";
+
+
+
+        echo "Testas, PHP testas" ;
+        echo "2Testas, PHP 2" ;
+
+    ?>
+
+
+    <h1>Testas</h1>
+    <p>Lokalus serveris</p>
+
+    <script>
+        "use strict";
+        console.log("Labas testas");
+    </script>
+</body>
+</html>
